cca8a95219527d2b1c14aa050d8bd0f2da5ab642
NLi2027 <gptabsolutemode@gmail.com>
2025-11-25 15:15:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById("navbar").innerHTML=`
     <nav>
-       <h3> [Home] | [Habitat] </h3>
+       <h3> <a href = "index.html">Home</a> | <a href = "carti.html">Habitat</a> </h3>
     </nav>
 
 
